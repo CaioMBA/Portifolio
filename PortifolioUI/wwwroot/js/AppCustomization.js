3247be4e015c536f470dd7267cca4474e02f8e95
Caio <caiombaraujo@gmail.com>
2024-09-22 23:21:50 -0300

    Responsive to the top

---
+++ PortifolioUI/wwwroot/js/AppCustomization.js
@@ -0,0 +1,30 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    enablingTypingAnimation();
+});
+
+/*#region TypeAnymation */
+function enablingTypingAnimation() {
+    var typed = new Typed(".typing", {
+        strings: ["Software Engineer", "Software Architect", "Tech Leader"],
+        typeSpeed: 20,
+        backSpeed: 50,
+        loop: true
+    });
+}
+
+/*#endregion TypeAnymation*/
+
+/*#region side-bar */
+const navToggleButton = document.querySelector(".side-bar .navigation-toggler");
+const sideBar = document.querySelector(".side-bar");
+
+navToggleButton.addEventListener("click", () => {
+    sideBarSectionTogglerBtn();
+});
+
+function sideBarSectionTogglerBtn() {
+    sideBar.classList.toggle("open");
+    navToggleButton.classList.toggle("open");
+}
+
+/*#endregion side-bar */
