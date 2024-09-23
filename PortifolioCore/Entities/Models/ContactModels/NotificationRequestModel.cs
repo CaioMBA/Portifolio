54191c390142abec553029bd0e0f71ad74600915
Caio <caiombaraujo@gmail.com>
2024-09-22 23:42:27 -0300

    Add notify dao

---
+++ PortifolioCore/Entities/Models/ContactModels/NotificationRequestModel.cs
@@ -0,0 +1,27 @@
+﻿namespace PortifolioCore.Entities.Models.ContactModels
+{
+    public class NotificationRequestModel
+    {
+        public List<MailModel>? mails;
+        public List<PhoneModel>? phones;
+    }
+
+    public class MailModel
+    {
+        public required List<string> mailDestinations { get; set; }
+        public required string subject { get; set; }
+        public required string msg { get; set; }
+        public List<AttachmentModel>? attachments { get; set; }
+    }
+
+    public class AttachmentModel
+    {
+        public required string fileName { get; set; }
+        public required string base64File { get; set; }
+    }
+
+    public class PhoneModel
+    {
+        public string? phoneNumber { get; set; }
+    }
+}
---
+++ PortifolioInfrastructure/Data/APIs/RestfulRepository/NotifyDAO.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using PortifolioCore.Auxiliary;
+using PortifolioCore.Entities.Models.ContactModels;
+using PortifolioCore.Entities.Models.GeneralSettingsModels;
+using PortifolioCore.Extensions;
+
+namespace PortifolioInfrastructure.Data.APIs.RestfulRepository
+{
+    public class NotifyDAO
+    {
+        private readonly ApiDefaultAccess _api;
+        private readonly Utils _utils;
+        public NotifyDAO(ApiDefaultAccess api, Utils utils)
+        {
+            _api = api;
+            _utils = utils;
+        }
+
+        public string sendNotification(NotificationRequestModel model)
+        {
+            ApiConnection endpoint = _utils.GetApi("Notifications");
+
+            ApiRequestModel request = new ApiRequestModel()
+            {
+                Url = endpoint.Url,
+                TypeRequest = endpoint.Type,
+                Auth = null,
+                Headers = null,
+                TimeOut = 10,
+                Body = model.ToJson()
+            };
+
+            var response = _api.ApiRequest(request);
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
